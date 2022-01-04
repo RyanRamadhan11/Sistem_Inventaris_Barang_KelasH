757af3994058e28a39aa6382e198d781cdcbe1a6
RilianYG <97087589+RilianYG@users.noreply.github.com>
2022-01-04 15:32:22 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,124 @@
+
+<br>
+
+       <!-- Begin Page Content -->
+        <div class="container-fluid">
+
+          <!-- Page Heading -->
+          <div class="d-sm-flex align-items-center justify-content-between mb-4">
+            <h1 class="h1 mb-0 text-gray-800">Dashboard Inventaris Barang (Toko Kelas H)</h1>
+           
+          </div>
+	 <marquee><h2>Selamat Datang di Sistem Informasi Inventaris Barang Kelompok 2 Kelas H</marquee></h2>
+		  <br></br>
+
+          <!-- Content Row -->
+          <div class="row">
+
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <a  href="?page=pengguna"> <div class="text-xs font-weight-bold text-info text-uppercase mb-1"><h4>Data Pengguna</h4></div>
+                      
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-calendar fa-2x text-black-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+			
+			
+			  <div class="col-xl- col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                     <a  href="?page=supplier"> <div class="text-xs font-weight-bold text-info text-uppercase mb-1"><h4>Data Supplier</h4></div>
+                      <div class="row no-gutters align-items-center">
+                        <div class="col-auto">
+                        
+                        </div>
+                        <div class="col">
+                         
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-clipboard-list fa-2x text-black-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+			
+			
+            <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                     <a  href="?page=gudang"> <div class="text-xs font-weight-bold text-info text-uppercase mb-1"><h4>Data Gudang</h4></div>
+                      <div class="row no-gutters align-items-center">
+                        <div class="col-auto">
+                        
+                        </div>
+                        <div class="col">
+                         
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-clipboard-list fa-2x text-black-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+			
+			 <!-- Earnings (Monthly) Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                        <a  href="?page=barangmasuk"> <div class="text-xs font-weight-bold text-info text-uppercase mb-1"><h4>Barang Masuk</h4></div>
+                   
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-dollar-sign fa-2x text-black-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <!-- Pending Requests Card Example -->
+            <div class="col-xl-3 col-md-6 mb-4">
+              <div class="card border-left-primary shadow h-100 py-2">
+                <div class="card-body">
+                  <div class="row no-gutters align-items-center">
+                    <div class="col mr-2">
+                     <a  href="?page=barangkeluar"> <div class="text-xs font-weight-bold text-info text-uppercase mb-1"><h4>Barang Keluar</h4></div>
+           
+                    </div>
+                    <div class="col-auto">
+                      <i class="fas fa-comments fa-2x text-black-300"></i>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+			
+			
+		
+		
+			
+			
+          </div>
