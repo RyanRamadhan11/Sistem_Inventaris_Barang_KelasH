a8885d98ac845757dc7e7f07360876930d9a101a
Ryan Ramadhan <92355043+RyanRamadhan11@users.noreply.github.com>
2022-03-08 11:25:42 +0700

    Update indexpetugas.php

--- indexpetugas.php
+++ indexpetugas.php
@@ -4,8 +4,6 @@ mysqli_report (MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 	error_reporting(E_ALL ^ (E_NOTICE | E_WARNING));
 	session_start();
 	
-	
-	
 	$koneksi = new mysqli("localhost","root","","inventaris");
 	
 	 if(empty($_SESSION['petugas'])){
@@ -13,11 +11,7 @@ mysqli_report (MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     header("location:login.php");
   }
 
-
-	
-	
-	
-	?>	
+?>	
 
 
 
@@ -316,10 +310,10 @@ mysqli_report (MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 			   
 			     
 			   if ($page == "") {
-				   include "homepetugas.php";
+				   include "homepetugas.html";
 			   }
 			   if ($page == "homepetugas") {
-				   include "homepetugas.php";
+				   include "homepetugas.html";
 			   }
 			   ?>
     
